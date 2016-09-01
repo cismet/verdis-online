e04d8c2df30c2c86ab1d54c108853f921f6b77dc
helllth <th@cismet.de>
2016-09-01 21:09:20 +0200

    Add some content for the first functionality

---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>verdis 2 go</h1>
+
+      <h2>First Step</h2>
+
+    </div>
+  );
+};
+
+export default HomePage;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,10 @@
 // Set up your root reducer here...
  import { combineReducers } from 'redux';
- export default combineReducers;
+ import {routerReducer} from 'react-router-redux';
+
+// export default combineReducers;
+
+ const rootReducer = combineReducers({
+  routing: routerReducer
+});
+export default rootReducer;
