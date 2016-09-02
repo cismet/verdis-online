25c1d4ba5b33c1d3dacb20f7f3d3d118f98f23b7
helllth <th@cismet.de>
2016-09-02 16:01:03 +0200

    working on #2: removed the map style fomr the index.ejs and moved it to a style object in the component file

--- src/components/VerdisMap.js
+++ src/components/VerdisMap.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { render } from 'react-dom';
+//import { render } from 'react-dom';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const position = [51.272399, 7.199712];
 
+const mapStyle = {
+  width: "100%",
+  height: 500
+};
+
 const VerdisMap = () => {
   return (
-
-    <Map center={position} zoom={18}>
+    <Map style={mapStyle} center={position} zoom={18}>
         <TileLayer
-          url='http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png'
+          url="http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           maxZoom={22}
         />
