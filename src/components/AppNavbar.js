766bd7c3290048d4e786c0bd7f5b96846cdf766f
helllth <th@cismet.de>
2016-09-20 11:43:17 +0200

    working on #4: extracting the AppNavbar out of Layout

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -9,20 +9,44 @@ import * as UiStateActions from '../actions/uiStateActions';
 
 function mapStateToProps(state) {
   return {
-    st:state
+    uiState:state.uiState
   };
 }
-
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(UiStateActions, dispatch)
-  };
+    uiActions: bindActionCreators(UiStateActions, dispatch)
+  }; 
 }
 
 export class AppNavbar_ extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.toggleInfo = this.toggleInfo.bind(this);
+    this.toggleCharts = this.toggleCharts.bind(this);
+    this.toggleKanal = this.toggleKanal.bind(this);
+    this.toggleFilter = this.toggleFilter.bind(this);
+    this.toggleDetails = this.toggleDetails.bind(this);
+  }
+  toggleInfo() {
+    this.props.uiActions.toggleInfoElements();
+  }
+  toggleCharts() {
+    this.props.uiActions.toggleChartsElements();
+  }
+  toggleKanal() {
+       this.props.uiActions.toggleKanalElements();
+  }
+  toggleFilter() {
+       this.props.uiActions.toggleFilterElements();
+  }
+  toggleDetails() {
+       this.props.uiActions.toggleDetailsElements();
+  }
+
   render() {
+    //console.log("Props:",this.props);
     return (
-        <Navbar inverse style={{marginBottom:0}}>
+    <Navbar inverse style={{marginBottom:0}}>
           <Navbar.Header>
             <Navbar.Brand>
               <LinkContainer to={{ pathname: '/' }}>
@@ -40,19 +64,19 @@ export class AppNavbar_ extends React.Component {
                 <MenuItem eventKey={1.4}>Versickerung</MenuItem>
               </NavDropdown>
             </Nav>
-            <Nav pullRight >
+            <Nav pullRight>
               <NavItem eventKey={2.0} >Info:</NavItem>
-              <NavItem active= "true" eventKey={2.1} href="#"><FontAwesome name="info-circle" size="1x"/></NavItem>
-              <NavItem active= "true" eventKey={2.2} href="#"><FontAwesome name="pie-chart" size="1x"/></NavItem>
-              <NavItem eventKey={2.3} href="#"><FontAwesome name="tint" size="1x"/></NavItem>
-              <NavItem eventKey={2.4} href="#"><FontAwesome name="filter" size="1x"/></NavItem>
-              <NavItem active= "true" eventKey={2.5} href="#"><FontAwesome name="th-list" size="1x"/></NavItem>
+              <NavItem className={(this.props.uiState.infoElementsEnabled)?"active":""} eventKey={2.1} href="#" onSelect={this.toggleInfo} ><FontAwesome name="info-circle" size="1x"/></NavItem>
+              <NavItem className={(this.props.uiState.chartElementsEnabled)?"active":""} eventKey={2.2} href="#" onSelect={this.toggleCharts} ><FontAwesome name="pie-chart" size="1x"/></NavItem>
+              <NavItem className={(this.props.uiState.kanalElementsEnabled)?"active":""} eventKey={2.3} href="#" onSelect={this.toggleKanal} ><FontAwesome name="tint" size="1x"/></NavItem>
+              <NavItem className={(this.props.uiState.filterElementEnabled)?"active":""} eventKey={2.4} href="#" onSelect={this.toggleFilter} ><FontAwesome name="filter" size="1x"/></NavItem>
+              <NavItem className={(this.props.uiState.detailElementsEnabled)?"active":""} eventKey={2.5} href="#" onSelect={this.toggleDetails} ><FontAwesome name="th-list" size="1x"/></NavItem>
               <LinkContainer to={{ pathname: '/foo', query: { bar: 'baz' } }}>
                 <NavItem eventKey={1} href="#"><Glyphicon glyph="user" /> SteinbacherD102</NavItem>
               </LinkContainer>
             </Nav>
           </Navbar.Collapse>
-        </Navbar>
+        </Navbar>        
     );
   }
 }
@@ -61,5 +85,6 @@ const AppNavbar = connect(mapStateToProps,mapDispatchToProps)(AppNavbar_);
 export default AppNavbar;
 
 AppNavbar_.propTypes = {
-  st:PropTypes.object
+  uiActions: PropTypes.object,
+  uiState: PropTypes.object
 };
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,100 +1,22 @@
 import React, { PropTypes } from 'react';
 import { connect } from "react-redux";
-import { bindActionCreators } from 'redux';
-import { Navbar , Nav, NavItem, NavDropdown, MenuItem, Glyphicon }  from 'react-bootstrap';
-import {LinkContainer} from 'react-router-bootstrap';
-//import AppNavbar from './AppNavbar';
-const FontAwesome = require('react-fontawesome');
-import * as UiStateActions from '../actions/uiStateActions';
-
-function mapStateToProps(state) {
-  return {
-    uiState:state.uiState
-  };
-}
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(UiStateActions, dispatch)
-  }; 
-}
+import AppNavbar from './AppNavbar';
 
 
 export class Layout_ extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.toggleInfo = this.toggleInfo.bind(this);
-    this.toggleCharts = this.toggleCharts.bind(this);
-    this.toggleKanal = this.toggleKanal.bind(this);
-    this.toggleFilter = this.toggleFilter.bind(this);
-    this.toggleDetails = this.toggleDetails.bind(this);
-  }
-  toggleInfo() {
-    console.log('toggleInfo props=',this.props);
-    this.props.actions.toggleInfoElements();
-  }
-  toggleCharts() {
-    console.log('toggleCharts');
-    this.props.actions.toggleChartsElements();
-
-  }
-  toggleKanal() {
-       this.props.actions.toggleKanalElements();
-  }
-  toggleFilter() {
-       this.props.actions.toggleFilterElements();
-  }
-  toggleDetails() {
-       this.props.actions.toggleDetailsElements();
-  }
-
-  
-  render() {
-    console.log("Props:",this.props);
-    console.log("infoElementsEnabled:",this.props.uiState.infoElementsEnabled);
+  render(){
     return (
-      <div>
-    <Navbar inverse style={{marginBottom:0}}>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <LinkContainer to={{ pathname: '/' }}>
-                <a >VerDIS</a>
-              </LinkContainer>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav pullLeft>
-              <NavDropdown eventKey={1} title="Versiegelte Flächen" id="basic-nav-dropdown">
-                <MenuItem eventKey={1.1}>Versiegelte Flächen</MenuItem>
-                <MenuItem eventKey={1.2}>ESW</MenuItem>
-                <MenuItem eventKey={1.3}>Info</MenuItem>
-                <MenuItem eventKey={1.4}>Versickerung</MenuItem>
-              </NavDropdown>
-            </Nav>
-            <Nav pullRight>
-              <NavItem eventKey={2.0} >Info:</NavItem>
-              <NavItem className={(this.props.uiState.infoElementsEnabled)?"active":""} eventKey={2.1} href="#" onSelect={this.toggleInfo} ><FontAwesome name="info-circle" size="1x"/></NavItem>
-              <NavItem className={(this.props.uiState.chartElementsEnabled)?"active":""} eventKey={2.2} href="#" onSelect={this.toggleCharts} ><FontAwesome name="pie-chart" size="1x"/></NavItem>
-              <NavItem className={(this.props.uiState.kanalElementsEnabled)?"active":""} eventKey={2.3} href="#" onSelect={this.toggleKanal} ><FontAwesome name="tint" size="1x"/></NavItem>
-              <NavItem className={(this.props.uiState.filterElementEnabled)?"active":""} eventKey={2.4} href="#" onSelect={this.toggleFilter} ><FontAwesome name="filter" size="1x"/></NavItem>
-              <NavItem className={(this.props.uiState.detailElementsEnabled)?"active":""} eventKey={2.5} href="#" onSelect={this.toggleDetails} ><FontAwesome name="th-list" size="1x"/></NavItem>
-              <LinkContainer to={{ pathname: '/foo', query: { bar: 'baz' } }}>
-                <NavItem eventKey={1} href="#"><Glyphicon glyph="user" /> SteinbacherD102</NavItem>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>        
-        {this.props.children}
-      </div>
+        <div>
+          <AppNavbar/>
+          {this.props.children}
+        </div>
     );
   }
 }
 
-const Layout = connect(mapStateToProps,mapDispatchToProps)(Layout_);
+const Layout = connect()(Layout_);
 export default Layout;
 
 Layout_.propTypes = {
-  children: PropTypes.element,
-  actions: PropTypes.object,
-  uiState: PropTypes.object
+  children: PropTypes.element
 };
