191d9348664b69289bf460b0dedc396b013a3815
helllth <th@cismet.de>
2017-02-22 18:26:41 +0100

    working on #10: show the optionsdialog 1 second delayed and actionTypes better named as actionTypes (good remark fomr pascal :-) )

--- src/actions/uiStateActions.js
+++ src/actions/uiStateActions.js
@@ -1,52 +1,76 @@
-import * as types from '../constants/actionTypes';
+import * as actionTypes from '../constants/actionTypes';
 
 export function toggleInfoElements() {
     return {
-        type: types.TOGGLE_INFO_ELEMENTS
+        type: actionTypes.TOGGLE_INFO_ELEMENTS
     };
 }
 
 export function toggleChartsElements() {
     return {
-        type: types.TOGGLE_CHART_ELEMENTS
+        type: actionTypes.TOGGLE_CHART_ELEMENTS
     };
 }
 
 export function toggleKanalElements() {
     return {
-        type: types.TOGGLE_KANAL_ELEMENTS
+        type: actionTypes.TOGGLE_KANAL_ELEMENTS
     };
 }
 
 export function toggleFilterElements() {
     return {
-        type: types.TOGGLE_FILTER_ELEMENT
+        type: actionTypes.TOGGLE_FILTER_ELEMENT
     };
 }
 
 export function toggleDetailsElements() {
     return {
-        type: types.TOGGLE_DETAIL_ELEMENTS
+        type: actionTypes.TOGGLE_DETAIL_ELEMENTS
     };
 }
 
 export function showSettings(visible) {
+// console.log(".o.");
+//     return {
+//         type: actionTypes.SHOW_SETTINGS,
+//         visible
+//     };
+//
+    return function (dispatch) {
+    // thunks allow for pre-processing actions, calling apis, and dispatching multiple actions
+    // in this case at this point we could call a service that would persist the fuel savings
+    console.log(dispatch);
+    console.log("showSettings: " + visible);
+    setTimeout(() => {
+        console.log("ASYNC showSettings: " + visible);
+        dispatch(showSettingsX(visible));
+    }, 1000);
+  };
+
+}
+
+export function showSettingsX(visible) {
+    console.log("\\o/");
     return {
-        type: types.SHOW_SETTINGS,
+        type: actionTypes.SHOW_SETTINGS,
         visible
     };
+
+
+
 }
 
 export function changeLayerOpacitySetting(key,opacity) {
     return {
-        type: types.CHANGE_LAYER_OPACITY,
+        type: actionTypes.CHANGE_LAYER_OPACITY,
         key,
         opacity
     };
 }
 export function changeLayerEnabledSetting(key,enabled) {
     return {
-        type: types.CHANGE_LAYER_ENABLED,
+        type: actionTypes.CHANGE_LAYER_ENABLED,
         key,
         enabled
     };
