d878805a96cc0f24fe4f73a5ddc16a2279856b3f
helllth <th@cismet.de>
2017-02-22 20:13:35 +0100

    working on #11: removing the waiting dialog when showing the settings. object-loading with waiting dialog is enough ;-)

--- src/actions/uiStateActions.js
+++ src/actions/uiStateActions.js
@@ -31,30 +31,10 @@ export function toggleDetailsElements() {
 }
 
 export function showSettings(visible) {
-    return function (dispatch) {
-    // thunks allow for pre-processing actions, calling apis, and dispatching multiple actions
-    // in this case at this point we could call a service that would persist the fuel savings
-    if (visible==true) {
-      dispatch(showWaiting(true));
-      setTimeout(() => {
-          console.log("ASYNC showSettings: " + visible);
-          dispatch(showSettingsImmediately(visible));
-          dispatch(showWaiting(false));
-      }, 1000);
-    }
-    else {
-      dispatch(showSettingsImmediately(visible));
-    }
+  return {
+      type: actionTypes.SHOW_SETTINGS,
+      visible
   };
-
-}
-
-export function showSettingsImmediately(visible) {
-    console.log("\\o/");
-    return {
-        type: actionTypes.SHOW_SETTINGS,
-        visible
-    };
 }
 
 export function showWaiting(visible) {
