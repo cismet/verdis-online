68137af38efc33f102f02106e45e8a0a7de45b31
helllth <th@cismet.de>
2017-03-07 18:02:48 +0100

    working on #25: removed the verdis-html5 route

--- src/routes.jsx
+++ src/routes.jsx
@@ -9,7 +9,6 @@ import NotFoundPage from './components/NotFoundPage.jsx';
 export default (
   <Route path="/" component={Layout}>
     <IndexRoute component={HomePage} />
-    <Route path="verdis-html5" component={HomePage} />
     <Route path="about" component={AboutPage} />
     <Route path="*" component={NotFoundPage} />
   </Route>
