2adef5b8f52f13e4626f04cfa79235f231543006
helllth <th@cismet.de>
2017-03-08 15:47:02 +0100

    switch to live API

--- src/constants/cids.js
+++ src/constants/cids.js
@@ -1,4 +1,4 @@
 export const DOMAIN = "VERDIS_GRUNDIS";
 export const GEOM_CLASS = "GEOM";
 export const GEOM_FIELD = "geo_field";
-export const SERVICE = "https:/verdis-api.cismet.de";
+export const SERVICE = "https://verdis-api.cismet.de";
