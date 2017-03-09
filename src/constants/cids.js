47919255565e667388ee8fcedf02681d9a4f814e
helllth <th@cismet.de>
2017-03-09 12:45:01 +0100

    switch to livesystem

--- src/constants/cids.js
+++ src/constants/cids.js
@@ -1,5 +1,5 @@
 export const DOMAIN = "VERDIS_GRUNDIS";
 export const GEOM_CLASS = "GEOM";
 export const GEOM_FIELD = "geo_field";
-//export const SERVICE = "https://verdis-api.cismet.de";
-export const SERVICE = "http://192.168.178.69:8890";
+export const SERVICE = "https://verdis-api.cismet.de";
+//export const SERVICE = "http://192.168.178.69:8890";
