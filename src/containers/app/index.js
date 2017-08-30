2e700a80efd32396c21ff6942d45db88da0d47a6
helllth <th@cismet.de>
2017-08-30 17:54:48 +0200

    working on #32: eliminating warning

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link, Switch } from 'react-router-dom'
+import { Route,  Switch } from 'react-router-dom'
 import XHome from '../example-home'
 import XAbout from '../example-about'
 import NotFoundPage from '../../components/NotFoundPage.jsx';
