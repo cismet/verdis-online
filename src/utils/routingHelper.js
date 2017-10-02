1548f72c8ed3a033b43024361e78648bc82a79fc
helllth <th@cismet.de>
2017-10-02 16:00:21 +0200

    🐛  working on #35: improved modifyQueryPart

--- src/utils/routingHelper.js
+++ src/utils/routingHelper.js
@@ -1,24 +1,23 @@
 import objectAssign from 'object-assign';
 import 'url-search-params-polyfill';
 
-export function modifyQueryPart(search,modifiedParts) {
-    let query=new URLSearchParams(search);
-  let newQuery= objectAssign({}, query,modifiedParts );
-    let pNames=Object.getOwnPropertyNames(newQuery);
-    let querypart="?";
-  let first=true;
-  for (let nidx in pNames) {
-      let connector;
-      if (first) {
-          connector="";
-      }
-      else {
-          connector="&";
-      }
-      querypart=querypart+connector+pNames[nidx]+"="+newQuery[pNames[nidx]];
-      first=false;
-  }
-  return querypart;  
+export function modifyQueryPart(search, modifiedParts) {
+    let query = getQueryObject(search)
+    let newQuery = objectAssign( query, modifiedParts);
+    let pNames = Object.getOwnPropertyNames(newQuery);
+    let querypart = "?";
+    let first = true;
+    for (let nidx in pNames) {
+        let connector;
+        if (first) {
+            connector = "";
+        } else {
+            connector = "&";
+        }
+        querypart = querypart.concat(connector,pNames[nidx],"=",newQuery[pNames[nidx]]);
+        first = false;
+    }
+    return querypart;
 }
 
 export function getQueryObject(search) {
