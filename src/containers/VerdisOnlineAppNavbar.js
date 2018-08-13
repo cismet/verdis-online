1f0fc362255f96bc2339c525ca9e13dc6202807a
helllth <th@cismet.de>
2018-08-13 15:34:31 +0200

    :sparkles: working on #41: added some tooltips to the menu

--- src/containers/VerdisOnlineAppNavbar.js
+++ src/containers/VerdisOnlineAppNavbar.js
@@ -2,7 +2,8 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from "react-redux";
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, OverlayTrigger, Tooltip } from 'react-bootstrap';
+
 import { actions as UiStateActions } from '../redux/modules/uiState';
 import { actions as MappingActions } from '../redux/modules/mapping';
 import { actions as KassenzeichenActions } from '../redux/modules/kassenzeichen';
@@ -78,25 +79,43 @@ export class AppNavbar_ extends React.Component {
 
 
   render() {    
+    let kassenzeichennummer;
+    if (this.props.kassenzeichen.kassenzeichennummer8) {
+        kassenzeichennummer=" (Kassenzeichen: " + this.props.kassenzeichen.kassenzeichennummer8 +")";
+    }
+    else {
+        kassenzeichennummer="";
+    } 
     return (<div>
       <Navbar  inverse style={{ marginBottom: 0 }}>
+      <OverlayTrigger placement="bottom" overlay={(<Tooltip style={{zIndex: 3000000000}} id="prevtt">Kassenzeichen ganz auf der Karte darstellen</Tooltip>)}>
         <Navbar.Header>
           <Navbar.Brand>  
-              <a id="verdis_online_brand" style={{ cursor: "pointer"}} onClick={this.fitBounds}>VerDIS-online</a>
+              <a id="verdis_online_brand" style={{ cursor: "pointer"}} onClick={this.fitBounds}>VerDIS-online{kassenzeichennummer}</a>
           </Navbar.Brand>
+
           <Navbar.Toggle />
         </Navbar.Header>
+        </OverlayTrigger>
         <Navbar.Collapse>
         
           <Nav pullRight>
             <NavItem id="navitem_showSettings" onClick={this.showSettings} eventKey={2.0} >Hilfe & Einstellungen</NavItem>
-            <NavItem id="navitem_downloadFEB" onClick={this.downloadFEB} eventKey={2.3} ><Icon name="file-pdf-o" /></NavItem>
-            <NavItem id="navitem_infoElementsEnabled" className={(this.props.uiState.infoElementsEnabled) ? "active" : ""} eventKey={2.1} href="#" onSelect={this.toggleInfo} ><Icon name="info-circle" /></NavItem>
-            <NavItem id="navitem_chartElementsEnabled" className={(this.props.uiState.chartElementsEnabled) ? "active" : ""} eventKey={2.2} href="#" onSelect={this.toggleCharts} ><Icon name="pie-chart" /></NavItem>
-            {/* <NavItem disabled className={(this.props.uiState.kanalElementsEnabled) ? "active" : ""} eventKey={2.3} href="#" onSelect={this.toggleKanal} ><FontAwesome name="tint" /></NavItem>
-            <NavItem disabled className={(this.props.uiState.filterElementEnabled) ? "active" : ""} eventKey={2.4} href="#" onSelect={this.toggleFilter} ><FontAwesome name="filter" /></NavItem> */}
-            <NavItem id="navitem_detailElementsEnabled" className={(this.props.uiState.detailElementsEnabled) ? "active" : ""} eventKey={2.5} href="#" onSelect={this.toggleDetails} ><Icon name="th-list" /></NavItem>
-            <NavItem id="navitem_logout" eventKey={3} href="/#/" ><Icon name="power-off" /></NavItem>
+            <OverlayTrigger placement="bottom" overlay={(<Tooltip style={{zIndex: 3000000000}} id="prevtt">Flächenerfassungsbogen erzeugen (PDF)</Tooltip>)}>
+                <NavItem id="navitem_downloadFEB" onClick={this.downloadFEB} eventKey={2.3} ><Icon name="file-pdf-o" /></NavItem>
+            </OverlayTrigger>
+            <OverlayTrigger placement="bottom" overlay={(<Tooltip style={{zIndex: 3000000000}} id="prevtt">{(this.props.uiState.infoElementsEnabled) ? "Kassenzeichen-Info ausblenden" : "Kassenzeichen-Info einblenden"}</Tooltip>)}>
+                <NavItem id="navitem_infoElementsEnabled" className={(this.props.uiState.infoElementsEnabled) ? "active" : ""} eventKey={2.1} href="#" onSelect={this.toggleInfo} ><Icon name="info-circle" /></NavItem>
+            </OverlayTrigger>
+            <OverlayTrigger placement="bottom" overlay={(<Tooltip style={{zIndex: 3000000000}} id="prevtt">{(this.props.uiState.chartElementsEnabled) ? "Diagramm ausblenden" : "Diagramm einblenden"}</Tooltip>)}>
+                <NavItem id="navitem_chartElementsEnabled" className={(this.props.uiState.chartElementsEnabled) ? "active" : ""} eventKey={2.2} href="#" onSelect={this.toggleCharts} ><Icon name="pie-chart" /></NavItem>
+            </OverlayTrigger>
+            <OverlayTrigger placement="bottom" overlay={(<Tooltip style={{zIndex: 3000000000}} id="prevtt">{(this.props.uiState.detailElementsEnabled) ? "Teilflächenliste ausblenden" : "Teilflächenliste einblenden"}</Tooltip>)}>
+                <NavItem id="navitem_detailElementsEnabled" className={(this.props.uiState.detailElementsEnabled) ? "active" : ""} eventKey={2.5} href="#" onSelect={this.toggleDetails} ><Icon name="th-list" /></NavItem>
+            </OverlayTrigger>
+            <OverlayTrigger placement="bottom" overlay={(<Tooltip style={{zIndex: 3000000000}} id="prevtt">VerDIS-online beenden</Tooltip>)}>
+                <NavItem id="navitem_logout" eventKey={3} href="/#/" ><Icon name="power-off" /></NavItem>
+            </OverlayTrigger>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
