1ff3fdc9545e6d8fa6aac148397ae8e4dc39cd02
helllth <th@cismet.de>
2019-02-20 11:53:49 +0100

    :sparkles: working on #41: prepare release

--- src/constants/cids.js
+++ src/constants/cids.js
@@ -1,14 +1,15 @@
-export const DOMAIN = "VERDIS_GRUNDIS";
-export const GEOM_CLASS = "GEOM";
-export const GEOM_FIELD = "geo_field";
+export const DOMAIN = 'VERDIS_GRUNDIS';
+export const GEOM_CLASS = 'GEOM';
+export const GEOM_FIELD = 'geo_field';
 //export const SERVICE = "https://verdis-api.cismet.de";
 //export const SERVICE = "http://192.168.178.69:8890";
 //export const STAC_SERVICE = "http://192.168.178.69:8890";
 
+// export const SERVICE = "http://s10221:27065";
+// export const STAC_SERVICE = "http://s10221:27065";
 
-export const SERVICE = "http://s10221:27065";
-export const STAC_SERVICE = "http://s10221:27065";
-
+export const SERVICE = 'https://verdis-api.cismet.de';
+export const STAC_SERVICE = 'https://verdis-api.cismet.de';
 
 // export const SERVICE = "http://leo/api/rest/v1/verdis";
 // export const STAC_SERVICE = "http://leo/api/rest/v1/verdis";
