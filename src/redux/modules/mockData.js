77781a5ef7151936cafca744e719fb4be4b549a1
helllth <th@cismet.de>
2019-09-18 12:31:21 +0200

    :sparkles: wip #43: show and hide CR dialogue and extend kassz with CR mock object

---
+++ src/redux/modules/mockData.js
@@ -0,0 +1,63 @@
+export const mockchangerequests = {
+	'60432515': {
+		kassenzeichen: 60432515,
+		flaechen: {
+			'8': {
+				flaechenart: {
+					art: 'Gründachfläche',
+					art_abkuerzung: 'GDF'
+				},
+				pruefung: {
+					flaechenart: {
+						value: {
+							art: 'Gründachfläche',
+							art_abkuerzung: 'GDF'
+						},
+						von: 'SteinbacherD102',
+						timestamp: 47110815
+					}
+				}
+			}
+		},
+		nachrichten: {
+			buerger: {},
+			sachbearbeiter: {
+				nachricht: 'Hallo Bürger. Wenn etwas nicht stimmt, dann ändere es einfach ;-)',
+				timestamp: 1566915257854,
+				bubbleStyles: {
+					chatbubble: {
+						background: 'red'
+					}
+				}
+			},
+			next: {
+				buerger: {
+					nachricht: 'Da passt was nicht weil isso, siehe lustiges pdf !',
+					timestamp: 1566915257744,
+					anhang: 'http://meine.domain.de/lustiges.pdf'
+				},
+				sachbearbeiter: {
+					nachricht: 'Konnte nichts feststellen, alles in Ordnung.',
+					timestamp: 1566915257854
+				},
+				next: {
+					buerger: {
+						nachricht: 'Oh, falsches PDF, siehe richtiges pdf.',
+						timestamp: 1566915257858,
+						anhang: 'http://meine.domain.de/richtiges.pdf'
+					},
+					sachbearbeiter: {
+						nachricht: 'Ach so, verstehe. Alles Klar !',
+						timestamp: 1566915257858
+					},
+					next: {
+						buerger: {
+							nachricht: 'Geht doch, danke.',
+							timestamp: 1566915257858
+						}
+					}
+				}
+			}
+		}
+	}
+};
