8517bafce3cc8b4a9ceea17a469d63845a64ffd8
helllth <th@cismet.de>
2019-10-17 09:38:38 +0200

    :hammer: :mute: wip #43: cleaning up and removing log files

--- src/components/commons/IconForAttribute.js
+++ src/components/commons/IconForAttribute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Comp = ({ iconAttributeMap, value }) => {
-	return <FontAwesomeIcon icon={iconAttributeMap[value]} />;
+	return <span>{value !== undefined && <FontAwesomeIcon icon={iconAttributeMap[value]} />}</span>;
 };
 export default Comp;
