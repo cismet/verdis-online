6c7b1f92abcbe703b207de68e0bd474784d83cdc
helllth <th@cismet.de>
2020-06-26 18:57:47 +0200

    :hammer: wo #46:  refactoring in the FAQ section (less duplicated code)

---
+++ src/components/helpandsettings/Menu50FAQEntry.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Link } from 'react-scroll';
+import { Icon } from 'react-fa';
+
+const Comp = ({ id, title, containerId = 'myMenu', linkToKey = 'help', content }) => {
+	return (
+		<div>
+			<div name={id}>
+				<br />
+			</div>
+			<h4 id={id}>
+				{title}{' '}
+				<Link to={linkToKey} containerId={containerId} style={{ color: '#00000044' }}>
+					<Icon name={'arrow-circle-up'} />
+				</Link>
+			</h4>
+			<p>{content}</p>
+		</div>
+	);
+};
+
+export default Comp;
+Comp.propTypes = {
+	key: PropTypes.string,
+	title: PropTypes.string,
+	containerId: PropTypes.string,
+	linkToKey: PropTypes.string,
+	content: PropTypes.object
+};
---
+++ src/components/helpandsettings/Menu50FAQEntryLink.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Label } from 'react-bootstrap';
+import { Link } from 'react-scroll';
+
+const Comp = (props) => {
+	const { id, title, containerId = 'myMenu', bsStyle = 'default' } = props;
+	console.log('linkto ', props);
+
+	return (
+		<Link to={id} containerId={containerId} style={{ textDecoration: 'none' }}>
+			<Label style={{ cursor: 'pointer' }} bsStyle={bsStyle}>
+				{title}
+			</Label>{' '}
+		</Link>
+	);
+};
+
+export default Comp;
+Comp.propTypes = {
+	key: PropTypes.string,
+	title: PropTypes.string,
+	containerId: PropTypes.string,
+	bsStyle: PropTypes.string
+};
