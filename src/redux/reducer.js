b215c664249d0b0a27be3ae6bf6838bb93c1161d
helllth <th@cismet.de>
2021-05-03 16:40:14 +0200

    dont store the changeRequestsMenuVisible prop in localStorage to avoid a problem in safari

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,44 +1,44 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-import kassenzeichenReducer from './modules/kassenzeichen';
-import mappingReducer from './modules/mapping';
-import authReducer from './modules/auth';
-import uiStateReducer from './modules/uiState';
-import { persistReducer } from 'redux-persist';
-import localForage from 'localforage';
+import { combineReducers } from "redux";
+import { routerReducer } from "react-router-redux";
+import kassenzeichenReducer from "./modules/kassenzeichen";
+import mappingReducer from "./modules/mapping";
+import authReducer from "./modules/auth";
+import uiStateReducer from "./modules/uiState";
+import { persistReducer } from "redux-persist";
+import localForage from "localforage";
 
 const uiStateStorageConfig = {
-	key: 'uiState',
-	storage: localForage,
-	whitelist: [
-		'd3Available',
-		'settingsVisible',
-		'applicationMenuVisible',
-		'applicationMenuActiveKey',
-		'changeRequestEditViewVisible',
-		'changeRequestEditViewFlaeche',
-		'changeRequestEditViewCR',
-		'changeRequestsMenuVisible',
-		'changeRequestsEditMode'
-	]
+    key: "uiState",
+    storage: localForage,
+    whitelist: [
+        "d3Available",
+        "settingsVisible",
+        "applicationMenuVisible",
+        "applicationMenuActiveKey",
+        "changeRequestEditViewVisible",
+        "changeRequestEditViewFlaeche",
+        "changeRequestEditViewCR",
+        //"changeRequestsMenuVisible",
+        "changeRequestsEditMode"
+    ]
 };
 
 const authStorageConfig = {
-	key: 'auth',
-	storage: localForage,
-	whitelist: [ 'user', 'password', 'stac' ]
+    key: "auth",
+    storage: localForage,
+    whitelist: ["user", "password", "stac"]
 };
 const mappingStorageConfig = {
-	key: 'mapping',
-	storage: localForage,
-	whitelist: [ 'selectedBackgroundIndex' ]
+    key: "mapping",
+    storage: localForage,
+    whitelist: ["selectedBackgroundIndex"]
 };
 
 const rootReducer = combineReducers({
-	kassenzeichen: kassenzeichenReducer,
-	mapping: persistReducer(mappingStorageConfig, mappingReducer),
-	uiState: persistReducer(uiStateStorageConfig, uiStateReducer),
-	auth: persistReducer(authStorageConfig, authReducer),
-	routing: routerReducer
+    kassenzeichen: kassenzeichenReducer,
+    mapping: persistReducer(mappingStorageConfig, mappingReducer),
+    uiState: persistReducer(uiStateStorageConfig, uiStateReducer),
+    auth: persistReducer(authStorageConfig, authReducer),
+    routing: routerReducer
 });
 export default rootReducer;
