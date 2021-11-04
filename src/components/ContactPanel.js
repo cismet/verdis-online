2d6673e7ee35b6afcb1ab77596ed730a7492e3d8
helllth <th@cismet.de>
2021-11-04 11:09:32 +0100

    fixes problem with contact set to null

--- src/components/ContactPanel.js
+++ src/components/ContactPanel.js
@@ -1,16 +1,29 @@
-import PropTypes from "prop-types";
 import React from "react";
 import { Well } from "react-bootstrap";
 
 const ContactPanel = ({ contact }) => {
+    let _contact;
+    if (!contact) {
+        _contact = {
+            anrede: "Herr",
+            vorname: "Dirk",
+            nachname: "Steinbacher",
+            telefon: "+49-202-563 5794",
+            mail: "dirk.steinbacher@stadt.wuppertal.de",
+            image: "SteinbacherD102.png"
+        };
+    } else {
+        _contact = contact;
+    }
+
     let styleOverride = {
         marginBottom: "5px",
         width: "100%"
     };
 
-    if (contact.image) {
+    if (_contact.image) {
         styleOverride = Object.assign(styleOverride, {
-            backgroundImage: 'url("/images/contacts/'+contact.image+'")',
+            backgroundImage: 'url("/images/contacts/' + _contact.image + '")',
             backgroundRepeat: "no-repeat",
             backgroundSize: "64px 64px",
             backgroundPosition: "right 10px top 15px"
@@ -21,29 +34,16 @@ const ContactPanel = ({ contact }) => {
         <Well bsSize="small" style={styleOverride}>
             <h4>Ihr Ansprechpartner:</h4>
             <h5>
-                {contact.anrede} {contact.vorname} {contact.nachname}{" "}
+                {_contact.anrede} {_contact.vorname} {_contact.nachname}{" "}
             </h5>
             <h6>
-                Telefon: <a href={"tel:" + contact.telefon}>{contact.telefon} </a>
+                Telefon: <a href={"tel:" + _contact.telefon}>{_contact.telefon} </a>
             </h6>
             <h6>
-                eMail: <a href={"mailto:" + contact.mail}>{contact.mail} </a>
+                eMail: <a href={"mailto:" + _contact.mail}>{_contact.mail} </a>
             </h6>
         </Well>
     );
 };
-ContactPanel.propTypes = {
-    contact: PropTypes.object
-};
 
-ContactPanel.defaultProps = {
-    contact: {
-        anrede: "Herr",
-        vorname: "Dirk",
-        nachname: "Steinbacher",
-        telefon: "+49-202-563 5794",
-        mail: "dirk.steinbacher@stadt.wuppertal.de",
-        image: "SteinbacherD102.png"
-    }
-};
 export default ContactPanel;
